5c711e7d9fb76c3788fd6031bc533f52c458d44b
cjn <cjnbill@qq.com>
2015-11-27 17:24:24 +0800

    init

---
+++ tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,16 @@
+__author__ = 'cuijianing'
+import scrapy
+
+class ZhihuSpider(scrapy.Spider):
+    name = "zhihu"
+    # allowed_domains = ["dmoz.org"]
+    start_urls = [
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.zhihu.com/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
